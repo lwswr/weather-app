d1f10de84524a5aa14ed65693417a67647d49057
Lewis Wear <lewiswear@Lewiss-MBP.home>
2020-07-30 15:55:51 +0100

    Updates

---
+++ precache-manifest.b205fdfd20d2b50c576d16a168da6ac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89faa8185e1c3ae9b517ba42b8602af7",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "b190a06b0ac74a71c146",
+    "url": "/weather-app/static/js/2.f90a56b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.f90a56b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0b04bd9e2b180b57a1",
+    "url": "/weather-app/static/js/main.63dd9c63.chunk.js"
+  },
+  {
+    "revision": "b583aba1370634b5a5a2",
+    "url": "/weather-app/static/js/runtime-main.60cbc992.js"
+  }
+]);
